adc69b4d1dfa569030af25bdb8a01829ff8e2503
nanramirez <nan.php.mx@gmail.com>
2024-02-24 23:11:41 -0600

    Diseño modal del formulario de nuevo usuario

--- app/Views/users/manage.php
+++ app/Views/users/manage.php
@@ -11,7 +11,7 @@
             <div id="cover_buttons_interaction_bar">
 					<?php echo anchor(url_title(Lang($controller_name.'.nav'))."/Form/-1",
 				"<div class='buttons_interaction_bar' title='Nuevo Usuario' > <span>Nuevo Usuario</span></div>",
-				array('class'=>'ajax-popup-link','data-toggle'=>'modal','data-target'=>'#form'));
+				array('class'=>'ajax-popup-link','data-toggle'=>'modal','data-target'=>'#form','data-whatever'=>'Nuevo usuario'));
 				?>
 			</div>
         </div>
@@ -22,5 +22,46 @@
             <?= $links ?>
         </div>
     </div>
+</div>
+<div class="modal fade" id="form" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Nombre:</label>
+            <input type="text" class="form-control" id="first_name" name="in_first_name" placeholder="Escriba el nombre">
+          </div>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Apellido:</label>
+            <input type="text" class="form-control" id="last_name" name="in_last_name" placeholder="Escriba el apellido">
+          </div>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label"> Usuario:</label>
+            <input type="text" class="form-control" id="username" name="in_username" placeholder="Escriba el nombre de usuario">
+          </div>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Contraseña</label>
+            <input type="text" class="form-control" id="password" name="in_password" placeholder="Escriba la contraseña">
+          </div>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Confirmar Contraseña</label>
+            <input type="text" class="form-control" id="repeat_password" name="repeat_password" placeholder="Confirme la contraseña">
+          </div>
+         
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary"> Guardar</button>
+      </div>
+    </div>
+  </div>
 </div>
 <?php echo view("footer"); ?>
